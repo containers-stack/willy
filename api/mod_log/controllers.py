9a871f2f0fd0811067bdbd2edfc58d2bfa55c07a
moti malka <motim@polytex.co.il>
2021-11-03 16:37:14 +0200

    Get logs by container id

--- api/mod_log/controllers.py
+++ api/mod_log/controllers.py
@@ -8,8 +8,8 @@ mod_log = Blueprint('log', __name__, url_prefix='')
 
 # Stream container log by id
 @socketio.on('stream_logs_request')
-def stream_logs_request():
-    container_logs =  Sdk.docker_client.logs('edc913ce90bc7a5503e109bc720989382acad3946dbc9dc88fc1b693a5b1fb13', stream=True, timestamps=True)
+def stream_logs_request(id):
+    container_logs =  Sdk.docker_client.logs(id, stream=True, timestamps=True)
     while True:
         try:
             line =  next(container_logs).decode("utf-8")
