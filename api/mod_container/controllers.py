81359a0efceb0732562a698aaa426f794b2934fd
Shmuel Raichman <shmuel@raichman.io>
2021-11-05 00:22:56 +0200

    Clean

--- api/mod_container/controllers.py
+++ api/mod_container/controllers.py
@@ -1,18 +1,16 @@
 # B"H
-# Import flask dependencies
-from os import name
-from typing import Container
-
-from docker.api import container
-from flask import Blueprint, request, render_template, \
-    flash, g, session, redirect, url_for, jsonify, abort
-from marshmallow import Schema, fields
-import json
 
 import datetime
+from typing import Container
+from flask import Blueprint, request, jsonify, abort
+from marshmallow import Schema, fields
+# ---
 from api.mod_sdk.models import Sdk
 from api.mod_container.models import Container
 from api.mod_container.models import ContainerEncoder
+
+
+# ---
 
 
 class MySchema(Schema):
@@ -21,6 +19,7 @@ class MySchema(Schema):
 
 # Define the blueprint: 'container', set its url prefix: app.url/containers
 mod_container = Blueprint('container', __name__, url_prefix='/containers')
+schema_instance = MySchema()
 
 
 # Set the route and accepted methods
@@ -31,36 +30,27 @@ def list_containers():
     for container_instance in Sdk.docker_client.containers():
         containers.append(Container(id=container_instance.get('Id'),
                                     name=container_instance.get('Names'),
-                                    age=(datetime.datetime.fromtimestamp(container_instance.get('Created') / 1e3)),
+                                    age=(container_instance.get('Created')),
                                     status=container_instance.get('Status'))
                           )
-    co = Sdk.docker_client.containers()
-
-    return jsonify(co)
+    return ContainerEncoder().encode(containers)
 
 
-schema_instance = MySchema()
-
-
+# TODO Add comments
 @mod_container.route('/container', methods=['GET'])
 def get_container():
     container_id = request.args.get('id')
-    print('container_id: ', container_id)
+    # Validate request parameter
     errors = schema_instance.validate(request.args)  # <--fix here
     if errors:
         abort(400, str(errors))
 
+    # TODO add catch
     container_instance = Sdk.docker_client.containers(filters={'id': container_id})[0]
-    print("container: ", container_instance)
-    print("name: ", container_instance.get('Names')[0])
-    print("created: ", container_instance.get('Created'))
-    print("status: ", container_instance.get('Status'))
-    print("id", container_instance.get('Id'))
 
-    container_object = Container(id=container_instance.get('Id'),
-                                 name=container_instance.get('Names')[0],
-                                 # age=(datetime.datetime.fromtimestamp(container.get('Created') / 1e3)),
-                                 age=(container_instance.get('Created')),
-                                 status=container_instance.get('Status'))
+    container = Container(id=container_instance.get('Id'),
+                          name=container_instance.get('Names')[0],
+                          age=(container_instance.get('Created')),
+                          status=container_instance.get('Status'))
 
-    return ContainerEncoder().encode(container_object)
+    return ContainerEncoder().encode(container)
