bfd8e72832af4783c32b09051f72ac9ab6f9673d
moti malka <motim@polytex.co.il>
2021-11-13 22:39:48 +0200

    Add api container logs

--- api/mod_log/controllers.py
+++ api/mod_log/controllers.py
@@ -1,34 +1,37 @@
-from flask import session, Blueprint
+from flask import Blueprint
 from api.mod_sdk.models import Sdk
 from api import socketio
+from flask_socketio import join_room, leave_room, rooms
 
 # Define the blueprint: 'log', set its url prefix: app.url/logs
 mod_log = Blueprint('log', __name__, url_prefix='')
 
-
 # Stream container log by id
-@socketio.on('stream_logs_request')
-def stream_logs_request(id):
-    container_logs = Sdk.docker_client.logs(id, stream=True, timestamps=True)
+@socketio.on('join_log_request')
+def join_log_request(containerid, sessionid):
+    
+    container_logs = Sdk.docker_client.logs(containerid, stream=True, timestamps=True)
+    
+    room = sessionid;
+
+    join_room(room)
+
     while True:
         try:
             log = next(container_logs).decode("utf-8")
-            socketio.emit('stream_logs_response', {'log': log, 'containerid':id})
+        
+            socketio.emit('stream_logs_response', {'log': log, 'containerid':containerid}, to=sessionid)
+
             socketio.sleep(0)
+
         except StopIteration:
-            socketio.emit('stream_logs_response', {'log': 'CONTAINER NOT RUNNING','containerid':id }, )
+            socketio.emit('stream_logs_response', {'log': 'CONTAINER NOT RUNNING','containerid':containerid }, rooms=room)
             break
 
-
 # Stream container log by id
-# @socketio.on('stream_stats_request')
-# def stream_cpu_request(id):
-#     container_stats = Sdk.docker_client.stats(id, stream=True)
-#     while True:
-#         try:
-#             stats = next(container_stats).decode("utf-8")
-#             socketio.emit('stream_stats_response', {'stats': stats, 'containerid':id})
-#             socketio.sleep(0)
-#         except StopIteration:
-#             socketio.emit('stream_stats_response', {'data': 'CONTAINER NOT RUNNING','containerid':id})
-#             break
+@socketio.on('leav_log_request')
+def leav_log_request(sessionid):
+    
+    leave_room(sessionid)
+
+    print(f'user {sessionid} leave room')
